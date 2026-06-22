a4a75bdd04ba1bcd21e0b0c2807e0e28606df28f
qwel0rd <secondofall22@rambler.ru>
2026-06-22 16:07:05 +0500

    Программирование пользовательского элемента Student. Лукашевич

--- Items/Student.xaml.cs
+++ Items/Student.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReportGeneration_Lukashevich.Classes;
+using ReportGeneration_Lukashevich.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,64 @@ namespace ReportGeneration_Lukashevich.Items
     /// </summary>
     public partial class Student : UserControl
     {
+        private Main _main;
         public Student()
         {
             InitializeComponent();
+        }
+
+        internal Student(StudentContext student, Main mainPage)
+        {
+            InitializeComponent();
+            _main = mainPage;
+
+            TBFio.Text = $"({student.Lastname}) {student.Firstname}";
+            CBExplored.IsChecked = student.Expelled;
+
+            List<DisciplineContext> StudentDisciplines = _main.AllDisciplines.FindAll(
+                x => x.IdGroup == student.IdGroup);
+
+            int NecessarilyCount = 0;
+            int WorksCount = 0;
+            int DoneCount = 0;
+            int MissedCount = 0;
+
+            foreach (DisciplineContext StudentDiscipline in StudentDisciplines)
+            {
+                List<WorkContext> StudentWorks = _main.AllWorks.FindAll(x =>
+                    (x.IdType == 1 || x.IdType == 2 || x.IdType == 3) &&
+                    x.IdDiscipline == StudentDiscipline.Id);
+
+                NecessarilyCount += StudentWorks.Count;
+
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaluation = _main.AllEvaluation.Find(x =>
+                        x.IdWork == StudentWork.Id &&
+                        x.IdStudent == student.Id);
+
+                    if (Evaluation != null && Evaluation.Value.Trim() != "" && Evaluation.Value.Trim() != "2")
+                        DoneCount++;
+                }
+
+                StudentWorks = _main.AllWorks.FindAll(x => x.IdType != 4 && x.IdType != 3);
+                WorksCount += StudentWorks.Count;
+
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaluation = _main.AllEvaluation.Find(x =>
+                        x.IdWork == StudentWork.Id &&
+                        x.IdStudent == student.Id);
+
+                    if (Evaluation != null && Evaluation.Lateness.Trim() != "")
+                        MissedCount += Convert.ToInt32(Evaluation.Lateness);
+                }
+            }
+
+            doneWorkers.Value = (NecessarilyCount > 0) ? (100f / NecessarilyCount) * DoneCount : 0;
+            missedCount.Value = (WorksCount > 0) ? (100f / (WorksCount * 90f)) * MissedCount : 0;
+
+            TBGroup.Text = _main.AllGroups.Find(x => x.Id == student.IdGroup).Name;
         }
     }
 }
--- Pages/Main.xaml.cs
+++ Pages/Main.xaml.cs
@@ -1,28 +1,68 @@
-﻿using System;
+﻿using ReportGeneration_Lukashevich.Classes;
+using ReportGeneration_Lukashevich.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ReportGeneration_Lukashevich.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Main.xaml
-    /// </summary>
     public partial class Main : Page
     {
+        internal List<GroupContext> AllGroups = GroupContext.AllGroups();
+        internal List<StudentContext> AllStudents = StudentContext.GetAllStudent();
+        internal List<WorkContext> AllWorks = WorkContext.GetAllWork();
+        internal List<EvaluationContext> AllEvaluation = EvaluationContext.AllEvaluations();
+        internal List<DisciplineContext> AllDisciplines = DisciplineContext.AllDisciplines();
+
         public Main()
         {
             InitializeComponent();
+            CreateGroupUI();
+            CreateStudents(AllStudents);
+        }
+
+        public void CreateGroupUI()
+        {
+            foreach (GroupContext Group in AllGroups)
+                CBGroups.Items.Add(Group.Name);
+            CBGroups.Items.Add("Выберите");
+            CBGroups.SelectedIndex = CBGroups.Items.Count - 1;
+        }
+
+        internal void CreateStudents(List<StudentContext> StudentsList)
+        {
+            Parent.Children.Clear();
+            foreach (StudentContext Student in StudentsList)
+                Parent.Children.Add(new Items.Student(Student, this));
+        }
+
+        private void SelectGroup(object sender, SelectionChangedEventArgs e)
+        {
+            if (CBGroups.SelectedIndex != CBGroups.Items.Count - 1)
+            {
+                int IdGroup = AllGroups.Find(x => x.Name == CBGroups.SelectedItem.ToString()).Id;
+                CreateStudents(AllStudents.FindAll(x => x.IdGroup == IdGroup));
+            }
+        }
+
+        private void SelectStudents(object sender, KeyEventArgs e)
+        {
+            List<StudentContext> SearchStudent = AllStudents;
+            if (CBGroups.SelectedIndex != CBGroups.Items.Count - 1)
+            {
+                int IdGroup = AllGroups.Find(x => x.Name == CBGroups.SelectedItem.ToString()).Id;
+                SearchStudent = AllStudents.FindAll(x => x.IdGroup == IdGroup);
+            }
+            string filter = TBFIO.Text.Trim();
+            if (!string.IsNullOrEmpty(filter))
+                SearchStudent = SearchStudent.FindAll(x =>
+                    (x.Lastname + " " + x.Firstname).ToLower().Contains(filter.ToLower()));
+            CreateStudents(SearchStudent);
+        }
+
+        private void ReportGeneration(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Заглушка для генерации отчёта
         }
     }
 }
