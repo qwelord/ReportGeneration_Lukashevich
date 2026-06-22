a959ab3296a50bb5df90d4da35625e8a7da8d8a3
qwel0rd <secondofall22@rambler.ru>
2026-06-22 15:08:35 +0500

    Разработанный класс подключения к БД. Лукашевич

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ReportGeneration_Lukashevich.Classes.Common
+{
+    public class Connection
+    {
+        public static string config = "server=127.0.0.1;uid=root;pwd=root;database=journal;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
