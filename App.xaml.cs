4443a2eb3e1273e733557c64e276f0c8ee9c629c
qwel0rd <secondofall22@rambler.ru>
2026-06-22 13:29:20 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ReportGeneration_Lukashevich
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
